673bc70557e9a202cd092ad8861da489d2e06855
Onadeko Oluwateniola <onadekoteniolalillian@gmail.com>
2023-03-06 13:54:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Question 4
+
+
+
+
+
+// Question 5
+
+const presidentialCandidates = {
+    AAC: 'Omoyele Sowore',
+    ACCORD: 'Christopher Imumolen',
+    APC: 'Bola Tinubu Ahmed',
+    LP: 'Peter Obi',
+    NNPP: 'Rabiu Kwankwaso',
+    PDP: 'Atiku Abubakar',
+}
+for (const property in presidentialCandidates){
+    console.log(`${property} is the presidential candiidate of ${object[property]}`);
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+Question 4
+Different way to clone objects are:
+Using the spread method
+Using the object.assign() function method
